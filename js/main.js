bfdbaa0550031584a8649b7367ac9ab8a05cc3c8
eriktlu <36303097+eriktlu@users.noreply.github.com>
2022-09-13 11:21:43 +0300

    Test 2

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
-function scrollTrigger(selector, options = {}) {
-    let els = document.querySelectorAll(selector)
-    els = Array.from(els)
-    els.forEach(el => {
-        addObserver(el, options)
-    })
-}
+// function scrollTrigger(selector, options = {}) {
+//     let els = document.querySelectorAll(selector)
+//     els = Array.from(els)
+//     els.forEach(el => {
+//         addObserver(el, options)
+//     })
+// }
 
-function addObserver(el, options){
-    if(!('IntersectionObserver' in window)) {
-        if(options.cb){
-            options.cb(el)
-        } else{
-            entry.target.classList.add('active')
-        }
-        return
-    }
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting) {
-                if(options.cb) {
-                    options.cb(el)
-                } else {
-                    entry.target.classList.add('active')
-                }
+// function addObserver(el, options){
+//     if(!('IntersectionObserver' in window)) {
+//         if(options.cb){
+//             options.cb(el)
+//         } else{
+//             entry.target.classList.add('active')
+//         }
+//         return
+//     }
+//     let observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach(entry => {
+//             if(entry.isIntersecting) {
+//                 if(options.cb) {
+//                     options.cb(el)
+//                 } else {
+//                     entry.target.classList.add('active')
+//                 }
                 
-                observer.unobserve(entry.target)
-            }
-        })
-    }, options)
-    observer.observe(el)
-}
+//                 observer.unobserve(entry.target)
+//             }
+//         })
+//     }, options)
+//     observer.observe(el)
+// }
 
 // scrollTrigger('.contact-us h3', {
 //     rootMargin: '-100px',
